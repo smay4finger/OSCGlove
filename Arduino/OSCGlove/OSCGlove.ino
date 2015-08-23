c7db9efafd7171f905c6df71bcf2fe546b753454
Stefan May <smay@4finger.net>
2015-08-23 20:55:53 +0200

    added gyro

--- Arduino/OSCGlove/OSCGlove.ino
+++ Arduino/OSCGlove/OSCGlove.ino
@@ -1,41 +1,5 @@
-/************************************************************************
-* This program is free software; you can redistribute it and/or modify *
-* it under the terms of the GNU License V2.                            *
-* This program is distributed in the hope that it will be useful,      *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-* GNU General Public License, version 2 for more details               *
-*                                                                      *
-* Bare bones ADXL345 i2c example for Arduino 1.0                       *
-* by Jens C Brynildsen <http://www.flashgamer.com>                     *
-* This version is not reliant of any external lib                      *
-* (Adapted for Arduino 1.0 from http://code.google.com/p/adxl345driver)*
-*                                                                      *
-* Demonstrates use of ADXL345 (using the Sparkfun ADXL345 breakout)    *
-* with i2c communication. Datasheet:                                   *
-* http://www.sparkfun.com/datasheets/Sensors/Accelerometer/ADXL345.pdf *
-* If you need more advanced features such as freefall and tap          *
-* detection, check out:                                                *
-* https://github.com/jenschr/Arduino-libraries                         *
-***********************************************************************/
-
-// Cabling for i2c using Sparkfun breakout with an Arduino Uno / Duemilanove:
-// Arduino <-> Breakout board
-// Gnd      -  GND
-// 3.3v     -  VCC
-// 3.3v     -  CS
-// Analog 4 -  SDA
-// Analog 5 -  SLC
-
-// Cabling for i2c using Sparkfun breakout with an Arduino Mega / Mega ADK:
-// Arduino <-> Breakout board
-// Gnd      -  GND
-// 3.3v     -  VCC
-// 3.3v     -  CS
-// 20       -  SDA
-// 21       -  SLC
-
 #include <Wire.h>
+#include <ITG3200.h>
 
 #define DEVICE (0x53) // Device address as specified in data sheet 
 
@@ -50,6 +14,8 @@ char DATAY1 = 0x35;	//Y-Axis Data 1
 char DATAZ0 = 0x36;	//Z-Axis Data 0
 char DATAZ1 = 0x37;	//Z-Axis Data 1
 
+ITG3200 gyro;
+
 void setup()
 {
   while(!Serial);
@@ -57,6 +23,8 @@ void setup()
   
   Wire.begin();        // join i2c bus (address optional for master)
   
+  gyro.initialize();
+
   //Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
   writeTo(DATA_FORMAT, 0x01);
   //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
@@ -86,11 +54,11 @@ void readAccel() {
   Serial.print(",\"z\":");
   Serial.print(z);
   Serial.print("},\"gyro\":{\"x\":");
-  Serial.print(x);
+  Serial.print(gyro.x());
   Serial.print(",\"y\":");
-  Serial.print(y);
+  Serial.print(gyro.y());
   Serial.print(",\"z\":");
-  Serial.print(z);
+  Serial.print(gyro.z());
   Serial.println("}}");
 }
 
